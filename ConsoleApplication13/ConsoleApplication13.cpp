ee34613310231eaa7819501ca1b7bcd6a9e321e0
Александр Капара <a.kapara2006@gmail.com>
2022-06-07 18:34:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+//(1)---------------------------------
+//int main()
+//{
+//	int num;
+//
+//	cout << "Enter number: ";
+//	cin >> num;
+//
+//	for (int i = 1; i <= num; i++)
+//	{
+//		if (num % i == 0)
+//		{
+//			cout << i << "\n";
+//		}
+//	}
+//
+//}
+//------------------------------------
+
+
+//(2)---------------------------------
+//int main()
+//{
+//	int num;
+//	
+//	cout << "Enter number: ";
+//	cin >> num;
+//
+//	for (int i = 2; i <= num; i++)
+//	{
+//		if (num % i == 0) 
+//		{
+//			if (num != i) 
+//			{
+//				cout << "Not simple \n";
+//				break;
+//			}
+//			cout << "Simple\n";
+//			break;
+//		}
+//	}
+//}
+//------------------------------------
+
+
+//(3)---------------------------------
+//int main()
+//{
+//	int num;
+//
+//	cout << "Enter number: ";
+//	cin >> num;
+//
+//	while (num >= 10)
+//	{
+//		int a = num % 10;
+//		int b = num % 100 / 10;
+//
+//		if (a == b)
+//		{
+//			cout << "Similar numbers go in a raw";
+//			break;
+//		}
+//		num /= 10;
+//	}
+//}
+//------------------------------------
+
+
+//(4)---------------------------------
+//int main()
+//{
+//	int num;
+//
+//	cout << "Enter number: ";
+//	cin >> num;
+//
+//	while (num >= 0)
+//	{
+//		int a = num % 10;
+//		int b = num % 100 / 10;
+//
+//		if (a < b)
+//		{
+//			cout << "Numbers are not in non-decreasing order";
+//			break;
+//		}
+//		num /= 10;
+//	}
+//}
+//------------------------------------
+
+
+//(5)---------------------------------
+int main()
+{
+	char choice;
+
+	cout << "Guess number from 0 - 1000 \n";
+	cout << "U ready? [y / n] \n";
+	cin >> choice;
+
+	if (choice == 'y')
+	{
+		int min = 0;
+		int max = 1000;
+		int mid;
+		string choise;
+
+		while (max > min)
+		{
+			mid = min + (max - min) / 2;
+
+			cout << "Number more than " << mid << " or == " << mid << "? \n [y / n / ==] \n";
+			cin >> choise;
+
+			if (choise == "y")
+			{
+				min = mid + 1;
+			}
+			else if (choise == "n")
+			{
+				max = mid;
+			}
+			else if (choise == "==")
+			{
+				cout << "Ur number " << (min + (max - min) / 2) - min + min << endl;
+				break;
+			}
+		}
+		
+	}
+	else if (choice == 'n')
+	{
+		exit(1);
+	}
+}
+//------------------------------------
